6d5da38b8dfb62fa2112e6a0bff12aebd4ab4ace
Syazwan Jamil <42228200+msyazwan7@users.noreply.github.com>
2020-02-01 10:19:46 +0800

    Updates

---
+++ precache-manifest.aedf6fd20d6e55a3790583f90294d14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4a2032adaf250d9888e17dca1cfff5",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "e43448aa41d0cfad6751",
+    "url": "/todo-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "26118e1cbb14333a53ab",
+    "url": "/todo-app/static/js/2.bded7f3a.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/todo-app/static/js/2.bded7f3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e43448aa41d0cfad6751",
+    "url": "/todo-app/static/js/main.266a17c4.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
